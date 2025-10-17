507a102f1c8a37e487b1f5f46ded187fd0683d29
Illia Campos <campos.ilya@globaldev.tech>
2025-10-17 09:56:56 +0300

    remove unused method

--- src/miniplatform/utils.py
+++ src/miniplatform/utils.py
@@ -4,12 +4,6 @@ class TimeFactor:
     def __init__(self, value=0):
         self.value = value
 
-    def incr(self, value):
-        self.value += value
-
-    def decr(self, value):
-        self.value -= value
-
     def set(self, value):
         self.value = value
 
